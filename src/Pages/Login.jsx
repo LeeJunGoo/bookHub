2a5a62052fda215cd049bb255f75137d7654a021
기다림 <gidalimforsomeday@gmail.com>
2024-02-08 14:08:30 +0900

    세이브

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,71 @@
-import { bookData } from "../shared/mockData";
+import { useNavigate } from "react-router";
+import { userData } from "../shared/mockData";
+import styled from "styled-components";
 
-
+console.log(userData)
 
 function Login() {
-  console.log(bookData)
+
+  const navigate = useNavigate();
+
+  const goToJoinPage = () => {
+    navigate('/join')
+  }
+
+
+  return (
+    <>
+      <div>
+        <StHeader>로그인</StHeader>
+        <StMain>
+          <ul>
+            <li>
+              <label>이메일</label>
+              <input
+                type="text"
+                placeholder="이메일을 입력해주세요."
+              ></input>
+            </li>
+            <li>
+              <label>패스워드</label>
+              <input
+                type="password"
+                placeholder="패스워드를 입력해주세요."
+              ></input>
+            </li>
+            <button>로그인!</button>
+          </ul>
+          <StBtn onClick={goToJoinPage}>회원가입하기</StBtn>
+        </StMain>
+      </div>
+    </>)
 }
 
-
 export default Login;
+
+
+
+const StMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  min-width: 400px;
+`
+
+const StHeader = styled.header`
+  display: flex ;
+  justify-content: center;
+  width: 100vw;
+  height: 15vh;
+  min-width: 400px;
+`
+
+
+
+const StBtn = styled.button`
+  display: flex;
+  justify-content:center;
+
+`
---
+++ src/shared/redux/modules/books.js
@@ -0,0 +1,5 @@
+// import bookData from '../../mockData';
+
+// const initailState = {
+//   bookData
+// };
