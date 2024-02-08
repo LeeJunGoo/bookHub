022a04cd538d4ae533d01a731eed4f4477303ca7
andan <andante1104@naver.com>
2024-02-08 21:04:11 +0900

    더미데이터를 통한 댓글 데이터 구현 및  bookData에서 id별 도서 보여주기 구현

--- src/Pages/DetailPages.jsx
+++ src/Pages/DetailPages.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { bookData, userData } from '../shared/mockData';
+import styled from 'styled-components';
 
 function DetailPages() {
   // 사용자 게시글 입력 데이터
@@ -12,9 +13,9 @@ function DetailPages() {
   const { id } = useParams();
   //console.log(typeof id);
 
-  const reviewIsNowData = userData.filter((user) => user.islogin === false && user.review);
-  console.log(reviewIsNowData);
-  // console.log(reviewIsNowdata);
+  const reviewIsNowData = userPostViewData.filter((user) => user.isLoggedIn === false && user.review);
+  // console.log(reviewIsNowData);
+
   return (
     <>
       {/* 
@@ -25,52 +26,107 @@ function DetailPages() {
       {bookData
         .filter((data) => data.itemId === Number(id))
         .map((book) => (
-          <div key={book.itemId}>
-            <img src={book.coverSmallUrl} alt="" />
-            <div>
-              <p>{book.title}</p>
-              <p>{book.customerReviewRank}</p>
-            </div>
-            <div>
-              <p>{book.description}</p>
-              <p>{book.author}</p>
-              <p>{book.publisher}</p>
-              <p></p>
-            </div>
-          </div>
+          <DetailCard key={book.itemId}>
+            <DetailCardBody>
+              <DetailImg src={book.coverLargeUrl} alt="" />
+              <DetailImgInfo>
+                <DetailImgInfoHero>
+                  <DetailCardImgInfoPtag>{book.author}</DetailCardImgInfoPtag>
+                  <DetailCardImgInfoPtag>{book.title}</DetailCardImgInfoPtag>
+                  <br />
+                  <DetailCardImgInfoPtag>{book.publisher}</DetailCardImgInfoPtag>
+                  <DetailCardImgInfoPtag>{book.description}</DetailCardImgInfoPtag>
+                  <br />
+                </DetailImgInfoHero>
+              </DetailImgInfo>
+            </DetailCardBody>
+          </DetailCard>
         ))}
-      {/* 사용자 게시글 부분  */}
-      <form>
-        <input
+
+      {/* 사용자 게시글 입력  */}
+      <DetailPostInputForm>
+        <DetailPosInputFormInput
           type="text"
           value={postTitle}
           onChange={(event) => {
             setPostTitle(event.target.value);
           }}
           placeholder="제목"
-        />
-        <textarea
+        />{' '}
+        <br />
+        <DetailPosInputFormText
           type="text"
           value={postText}
           onChange={(event) => {
             setPostText(event.target.value);
           }}
           placeholder="내용"
-        />
-        <button type="submit"></button>
-      </form>
+        />{' '}
+        <br />
+        <DetailPostSubmitButton type="submit">추가하기</DetailPostSubmitButton>
+      </DetailPostInputForm>
+
+      {/* 댓글 다는 창 */}
 
       {reviewIsNowData.map((data) => (
-        <>
-          <div key={data.idx}>
-            <b>{data.userNickName}</b>
-
-            {data.review.map(())}
-          </div>
-        </>
+        <div key={data.idx}>
+          {data.review.map((d) => (
+            <>
+              <p>{d.reviewUser}</p>
+              <p>{d.content}</p>
+            </>
+          ))}
+        </div>
       ))}
     </>
   );
 }
 
 export default DetailPages;
+
+const DetailCard = styled.div`
+  width: 1158px;
+  height: 400px;
+`;
+
+const DetailImg = styled.img`
+  width: 220px;
+  height: 334px;
+`;
+
+const DetailCardBody = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+const DetailImgInfo = styled.div``;
+
+const DetailImgInfoHero = styled.div``;
+
+const DetailCardImgInfoPtag = styled.p`
+  font-weight: bolder;
+  margin: 10px;
+`;
+
+const DetailPostInputForm = styled.form`
+  margin-left: 10rem;
+`;
+
+const DetailPosInputFormInput = styled.input`
+  width: 500px;
+  height: 20px;
+`;
+
+const DetailPosInputFormText = styled.textarea`
+  margin-top: 10px;
+  width: 500px;
+  height: 80px;
+`;
+
+const DetailPostSubmitButton = styled.button`
+  margin-left: 440px;
+  margin-top: 10px;
+`;
+
+// 댓글 스타일 해야됨
