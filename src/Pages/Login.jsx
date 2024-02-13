c167ec217f6d11f8734a728d2a43256107279cc3
기다림 <gidalimforsomeday@gmail.com>
2024-02-13 21:14:32 +0900

    login브랜치 병합

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -19,17 +19,93 @@ function Login() {
 
   const navigate = useNavigate();
   const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const [email, setUserEmail] = useState('');
   const [password, setUserPwd] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const addGoogleAccount = async () => {
+    signInWithRedirect(auth, googleProvider);
+  }
+
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then(async (result) => {
+        console.log('result가 존재하는지', result)
+        if (result) {
+          const user = result.user;
+          const providerId = result.providerId;
+          const userRef = doc(db, 'users', user.uid)
+          const docSnap = await getDoc(userRef)
+
+          console.log('docSnap.exists 가 진짜 존재하는지', docSnap.exists())
+          if (docSnap.exists()) {
+            await updateDoc(userRef, {
+              lastLogin: new Date(),
+
+            });
+          } else {
+            let etc = '';
+            if (providerId === 'google.com') {
+              etc = 'Google로 로그인 한 회원입니다.';
+            } else if (providerId === 'github.com') {
+              etc = 'Github으로 로그인 한 회원입니다.';
+            }
+            await setDoc(userRef, {
+              uid: user.uid,
+              userNickName: user.displayName,
+              userEmail: user.email,
+              etc: etc,
+              created: new Date(),
+            });
+          }
+          alert(`${user.displayName ? `${user.displayName}님, 환영합니다!` : `환영합니다! 마이페이지에서 귀하의 닉네임을 설정해주시겠어요?`}`);
+          navigate('/')
+        }
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(`${errorMessage} 의 에러가 발생했습니다. 에러코드: ${errorCode}`)
+      });
+  }, [])
+
+
+  const addGithubAccount = async () => {
+    signInWithRedirect(auth, githubProvider);
+    getRedirectResult(auth)
+      .then(async (result) => {
+        const user = result.user;
+        await addDoc(collection(db, 'users'), {
+          uid: user.uid,
+          userNickName: user.displayName,
+          userEmail: user.email,
+          etc: 'Github 으로 로그인 한 회원입니다.'
+        });
+        setIsLoading(false)
+        alert(`깃허브를 통해 찾아주셔서 반갑습니다!`)
+        navigate('/')
+
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const userEmail = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        alert(`${errorMessage} 의 에러가 발생했습니다. 에러코드: ${errorCode}`)
+        console.log(` 작성된 email은 ${userEmail}, credentail은 ${credential}`)
+        // ...
+      })
+
+  }
+
 
   const loggedIn = async (e) => {
     e.preventDefault();
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user
-        console.log(userCredential)
-        console.log(`로그인이 완료됐습니다 id: ${email}, Uid ${user.uid}`)
+        alert(`환영합니다!`)
         navigate(`/`)
       })
 
@@ -84,11 +160,12 @@ function Login() {
             <div>
               <StBtn onClick={(e) => loggedIn(e)}>로그인!</StBtn>
             </div>
+            <button onClick={addGoogleAccount}>구글 로그인</button>
+            <button onClick={addGithubAccount}>깃허브 로그인</button>
           </StUl>
           <StBtn onClick={goToJoinPage}>회원가입하기</StBtn>
           <StBtn onClick={goToHome}>홈으로 가기</StBtn>
         </StMain>
-
       </div>
     </>)
 }
@@ -145,5 +222,13 @@ const StHeader = styled.header`
 const StBtn = styled.button`
   display: flex;
   justify-content:center;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  font-size: 1.6rem;
+  background-color: #d5ceff;
+  border: 1px solid #452ec4;
+  border-radius: 10px;
+  color: #4730cc;
 
 `
