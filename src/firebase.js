b524a8518dc0d13b3c40660983a64f7b2c0a70c3
andan <andante1104@naver.com>
2024-02-13 14:28:30 +0900

    detailPage 삭제

--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,17 @@ import { getStorage, ref } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: 'AIzaSyA2EX8iS-pKkX9K4cDzZb9zbfRC7mXcAfw',
+  authDomain: 'reactproj-3ea63.firebaseapp.com',
+  projectId: 'reactproj-3ea63',
+  storageBucket: 'reactproj-3ea63.appspot.com',
+  messagingSenderId: '907491810608',
+  appId: '1:907491810608:web:eafc33ce716c47a00ebcf5',
+  measurementId: 'G-NBPCPYFH27'
+};
+
+/*
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -15,6 +26,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: 'G-NBPCPYFH27'
 };
+
+ */
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
