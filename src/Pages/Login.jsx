a3452de566a190b7e5f8630ed3b7d617be69cd53
기다림 <gidalimforsomeday@gmail.com>
2024-02-13 14:59:50 +0900

    로그인, 마이페이지 갱신

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,90 +1,86 @@
-import { useNavigate } from 'react-router';
-import styled from 'styled-components';
-import { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from '@firebase/auth';
-import { useDispatch } from 'react-redux';
+import { useNavigate } from "react-router";
+import styled from "styled-components";
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "@firebase/auth";
 
 // id : gang@dev.com
 // pwd : 123123
 
 function Login() {
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const auth = getAuth();
-  const dispatch = useDispatch();
 
   const [email, setUserEmail] = useState('');
   const [password, setUserPwd] = useState('');
 
   const loggedIn = async (e) => {
-    console.log(email, password);
     e.preventDefault();
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(userCredential);
-        console.log(`로그인이 완료됐습니다 id: ${email}, Uid ${user.uid}`);
-        navigate(`/`);
+        const user = userCredential.user
+        console.log(userCredential)
+        console.log(`로그인이 완료됐습니다 id: ${email}, Uid ${user.uid}`)
+        navigate(`/`)
       })
 
       .catch((error) => {
         const errorCode = error.code;
+        const errorMessage = error.message
+        alert(`${errorMessage} 의 오류가 발생했습니다. 에러코드: ${errorCode}`)
+        navigate('/login')
+      })
 
-        const errorMessage = error.message;
-        navigate('/login');
-        alert(`${errorMessage} 의 오류가 발생했습니다. 에러코드: ${errorCode}`);
-      });
-  };
+  }
 
   const onUserEmailHandler = (e) => {
-    setUserEmail(e.target.value);
-  };
+    setUserEmail(e.target.value)
+  }
 
   const onUserPwdHandler = (e) => {
-    console.log(e);
-    setUserPwd(e.target.value);
-  };
+    setUserPwd(e.target.value)
+  }
 
   const goToJoinPage = () => {
-    navigate('/join');
-  };
+    navigate('/join')
+  }
 
   const goToHome = () => {
-    navigate('/');
-  };
+    navigate('/')
+  }
 
   return (
     <>
       <div>
-        <StHeader>로그인</StHeader>
+        <StHeader>로그인
+        </StHeader>
         <StMain>
           <StUl>
             <li>
-              <label>이메일</label>
               <input
-                type="email"
+                type="text"
+                placeholder="이메일"
                 value={email}
-                onChange={onUserEmailHandler}
-                placeholder="이메일을 입력해주세요."
+                onChange={(e) => onUserEmailHandler(e)}
               ></input>
             </li>
             <li>
-              <label>패스워드</label>
               <input
                 type="password"
+                placeholder="비밀번호"
                 value={password}
-                onChange={onUserPwdHandler}
-                placeholder="패스워드를 입력해주세요."
+                onChange={(e) => onUserPwdHandler(e)}
               ></input>
             </li>
-            <button onClick={(e) => loggedIn(e)}>로그인!</button>
+            <div>
+              <button onClick={(e) => loggedIn(e)}>로그인!</button>
+            </div>
           </StUl>
           <StBtn onClick={goToJoinPage}>회원가입하기</StBtn>
           <StBtn onClick={goToHome}>홈으로 가기</StBtn>
         </StMain>
       </div>
-    </>
-  );
+    </>)
 }
 
 export default Login;
@@ -96,27 +92,28 @@ const StMain = styled.main`
   align-items: center;
   width: 100vw;
   min-width: 400px;
-`;
-
-const StHeader = styled.header`
-  display: flex;
-  justify-content: center;
-  width: 100vw;
-  height: 15vh;
-  min-width: 400px;
-`;
-
-const StBtn = styled.button`
-  display: flex;
-  justify-content: center;
-`;
+`
 
 const StUl = styled.ul`
-  display: flex;
+  display : flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
   width: 300px;
   height: 200px;
   border: 1px solid black;
-`;
+`
+
+const StHeader = styled.header`
+  display: flex ;
+  justify-content: center;
+  width: 100vw;
+  height: 15vh;
+  min-width: 400px;
+`
+
+const StBtn = styled.button`
+  display: flex;
+  justify-content:center;
+
+`
