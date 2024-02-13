31dcab1da292b40d126fd806c775a360932a172c
기다림 <gidalimforsomeday@gmail.com>
2024-02-13 20:59:36 +0900

    소셜로그인 오류해결중

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,16 @@
 import { useNavigate } from "react-router";
 import styled from "styled-components";
-import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword, getRedirectResult, signInWithRedirect, GoogleAuthProvider } from "@firebase/auth";
+import { useState, useEffect } from "react";
+import { addDoc, collection, getDoc, doc, setDoc, query, where, getDocs, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  getRedirectResult,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "@firebase/auth";
 
 // id : gang@dev.com
 // pwd : 123123
@@ -10,47 +19,93 @@ function Login() {
 
   const navigate = useNavigate();
   const auth = getAuth();
-  // const provider = new GoogleAuthProvider();
-  // signInWithRedirect(auth, provider);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const [email, setUserEmail] = useState('');
   const [password, setUserPwd] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-  // getRedirectResult(auth)
-  //   .then((result) => {
-  //     // This gives you a Google Access Token. You can use it to access Google APIs.
-  //     const credential = GoogleAuthProvider.credentialFromResult(result);
-  //     const token = credential.accessToken;
+  const addGoogleAccount = async () => {
+    signInWithRedirect(auth, googleProvider);
+  }
 
-  //     // The signed-in user info.
-  //     const user = result.user;
-  //     // IdP data available using getAdditionalUserInfo(result)
-  //     // ...
-  //   }).catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     // The email of the user's account used.
-  //     const userEmail = error.customData.email;
-  //     // The AuthCredential type that was used.
-  //     const credential = GoogleAuthProvider.credentialFromError(error);
-  //     alert(`${errorMessage} 의 에러가 발생했습니다. 에러코드: ${errorCode}`)
-  //     console.log(` 작성된 email은 ${userEmail}, credentail은 ${credential}`)
-  //     // ...
-  //   });
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then(async (result) => {
+        console.log('result가 존재하는지', result)
+        if (result) {
+          const user = result.user;
+          const providerId = result.providerId;
+          const userRef = doc(db, 'users', user.uid)
+          const docSnap = await getDoc(userRef)
+
+          console.log('docSnap.exists 가 진짜 존재하는지', docSnap.exists())
+          if (docSnap.exists()) {
+            await updateDoc(userRef, {
+              lastLogin: new Date(),
+
+            });
+          } else {
+            let etc = '';
+            if (providerId === 'google.com') {
+              etc = 'Google로 로그인 한 회원입니다.';
+            } else if (providerId === 'github.com') {
+              etc = 'Github으로 로그인 한 회원입니다.';
+            }
+            await setDoc(userRef, {
+              uid: user.uid,
+              userNickName: user.displayName,
+              userEmail: user.email,
+              etc: etc,
+              created: new Date(),
+            });
+          }
+          alert(`${user.displayName ? `${user.displayName}님, 환영합니다!` : `환영합니다! 마이페이지에서 귀하의 닉네임을 설정해주시겠어요?`}`);
+          navigate('/')
+        }
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(`${errorMessage} 의 에러가 발생했습니다. 에러코드: ${errorCode}`)
+      });
+  }, [])
 
 
+  const addGithubAccount = async () => {
+    signInWithRedirect(auth, githubProvider);
+    getRedirectResult(auth)
+      .then(async (result) => {
+        const user = result.user;
+        await addDoc(collection(db, 'users'), {
+          uid: user.uid,
+          userNickName: user.displayName,
+          userEmail: user.email,
+          etc: 'Github 으로 로그인 한 회원입니다.'
+        });
+        setIsLoading(false)
+        alert(`깃허브를 통해 찾아주셔서 반갑습니다!`)
+        navigate('/')
 
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
 
+        const userEmail = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        alert(`${errorMessage} 의 에러가 발생했습니다. 에러코드: ${errorCode}`)
+        console.log(` 작성된 email은 ${userEmail}, credentail은 ${credential}`)
+        // ...
+      })
 
-
+  }
 
 
   const loggedIn = async (e) => {
     e.preventDefault();
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user
-        console.log('안녕하세요', user)
+        alert(`환영합니다!`)
         navigate(`/`)
       })
 
@@ -105,6 +160,8 @@ function Login() {
             <div>
               <StBtn onClick={(e) => loggedIn(e)}>로그인!</StBtn>
             </div>
+            <button onClick={addGoogleAccount}>구글 로그인</button>
+            <button onClick={addGithubAccount}>깃허브 로그인</button>
           </StUl>
           <StBtn onClick={goToJoinPage}>회원가입하기</StBtn>
           <StBtn onClick={goToHome}>홈으로 가기</StBtn>
@@ -165,6 +222,10 @@ const StHeader = styled.header`
 const StBtn = styled.button`
   display: flex;
   justify-content:center;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  font-size: 1.6rem;
   background-color: #d5ceff;
   border: 1px solid #452ec4;
   border-radius: 10px;
