d485f528b6c6f297ddab8564afa282d7ce714749
yuriyun88 <bwzfwnr092u@gmail.com>
2024-02-13 16:28:50 +0900

    오류

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -17,6 +17,19 @@ export const GlobalStyle = createGlobalStyle`
     font-style: normal;
 }
 
+@font-face {
+    font-family: 'MaruBuri-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10-21@1.0/MaruBuri-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'SOGANGUNIVERSITYTTF';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2312-1@1.1/SOGANGUNIVERSITYTTF.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
 
   
 html, body, div, span, applet, object, iframe,
